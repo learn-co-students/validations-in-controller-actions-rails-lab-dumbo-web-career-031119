e286e378fb0ccbb2e6ab88e64c7def4528a567a6
github username <itsDario1@gmail.com>
2019-04-08 19:38:23 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: :true
+  validates :content, length: { minimum: 10 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
 end
+
+# validates :size, inclusion: { in: %w(small medium large)
